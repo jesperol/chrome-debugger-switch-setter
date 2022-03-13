e4e8e386155d5c622d8c040236cb081befb6793c
Jesper Olsson <c.jesper.olsson@gmail.com>
2022-03-13 01:43:31 +0100

    Sending protocol command
    Popup and color select -> background set example also left in...

--- background.js
+++ background.js
@@ -1,3 +1,87 @@
-chrome.action.onClicked.addListener(tab => { 
-     
+/* chrome.runtime.onInstalled.addListener(() => {
+  let color = "#3aa757";
+  chrome.storage.sync.set({ color });
+  console.log('Stored background color set to %c%s', `color: ${color}`, color);
 });
+
+chrome.action.onClicked.addListener((tab) => {
+  if (!tab.url.includes("chrome://")) {
+    chrome.storage.sync.get("color", ({ color }) => {
+      chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        func: (c) => { document.body.style.backgroundColor = c; },
+        args: [color],
+      });
+      console.log('Document background color set to %c%s', `color: ${color}`, color);
+    })
+  }
+}); */
+
+chrome.action.onClicked.addListener((tab) => {
+  if (tab.url.includes("chrome://")) return;
+  debuggeeId = { tabId: tab.id };
+
+  // We need to attach to our tab first in order to be able to ssendCommand() to it
+  cb = onAttach.bind(null, debuggeeId);
+  chrome.debugger.attach(debuggeeId, "1.0", cb);
+
+/*   chrome.debugger.sendCommand(
+      debuggeeId,
+      "Emulation.setEmulatedMedia", {
+        "media": "",
+        "features": [
+            {
+                "name": "color-gamut",
+                "value": ""
+            },
+            {
+                "name": "prefers-color-scheme",
+                "value": "dark"
+            },
+            {
+                "name": "forced-colors",
+                "value": ""
+            },
+            {
+                "name": "prefers-contrast",
+                "value": ""
+            },
+            {
+                "name": "prefers-reduced-data",
+                "value": ""
+            },
+            {
+                "name": "prefers-reduced-motion",
+                "value": ""
+            }
+        ]
+      },
+      onSetEmulatedMedia.bind(null, debuggeeId)); */
+
+  chrome.debugger.sendCommand(
+    debuggeeId,
+    "Emulation.setAutoDarkModeOverride", { "enabled": true },
+    onSetAutoDarkOverride.bind(null, debuggeeId)
+  );
+});
+
+function onAttach(debuggeeId) {
+  console.log("onAttach: " + debuggeeId)
+  if (chrome.runtime.lastError) {
+    console.log(chrome.runtime.lastError.message);
+  }
+}
+
+function onSetAutoDarkOverride(debuggeeId) {
+  console.log("onAutoDarkOverride: " + debuggeeId);
+  if (chrome.runtime.lastError) {
+    console.log(chrome.runtime.lastError.message);
+  }
+}
+
+function onSetEmulatedMedia(debuggeeId) {
+  console.log("onEmulatedMedia: " + debuggeeId);
+  if (chrome.runtime.lastError) {
+    console.log(chrome.runtime.lastError.message);
+  }
+}
--- popup.js
+++ popup.js
@@ -1,102 +1,25 @@
-let injectFunction = document.getElementById('inject-function');
-var version = "1.0"
+// Initialize button with users' preferred color
+let changeColor = document.getElementById("changeColor");
 
-async function getCurrentTab() {
-  let queryOptions = { active: true, currentWindow: true };
-  let [tab] = await chrome.tabs.query(queryOptions);
-  chrome.debugger
-  return tab;
-}
-
-injectFunction.addEventListener('click', async () => {
-  let tab = await getCurrentTab();
-  debuggeeId = { tabId: tab.id }
-
-  // We need to attach to our tab first in order to be able to ssendCommand() to it
-  cb = onAttach.bind(null, debuggeeId)
-  chrome.debugger.attach(debuggeeId, version, cb);
-
-  function onAttach(debuggeeId) {
-    if (chrome.runtime.lastError) {
-      alert(chrome.runtime.lastError.message);
-      return;
-    }
-  }
-
-  chrome.debugger.sendCommand(
-      debuggeeId,
-      "Emulation.setEmulatedMedia", {
-        "media": "",
-        "features": [
-            {
-                "name": "color-gamut",
-                "value": ""
-            },
-            {
-                "name": "prefers-color-scheme",
-                "value": "dark"
-            },
-            {
-                "name": "forced-colors",
-                "value": ""
-            },
-            {
-                "name": "prefers-contrast",
-                "value": ""
-            },
-            {
-                "name": "prefers-reduced-data",
-                "value": ""
-            },
-            {
-                "name": "prefers-reduced-motion",
-                "value": ""
-            }
-        ]
-      },
-      onSetEmulatedMedia.bind(null, debuggeeId));
-
-  chrome.debugger.sendCommand(
-      debuggeeId,
-      "Emulation.setAutoDarkModeOverride", { "enabled": true },
-      onSetAutoDarkOverride.bind(null, debuggeeId));
+chrome.storage.sync.get("color", ({ color }) => {
+  changeColor.style.backgroundColor = color;
 });
 
-function onSetAutoDarkOverride(debuggeeId) {
-  console.log({ "onAutoDarkOverride": debuggeeId });
-}
+// When the button is clicked, inject setPageBackgroundColor into current page
+changeColor.addEventListener("click", async () => {
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
-function onSetEmulatedMedia(debuggeeId) {
-  console.log({ "onEmulatedMedia": debuggeeId });
-}
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    function: setPageBackgroundColor,
+  });
+});
 
-
-chrome.debugger.onEvent.addListener(onEvent);
-
-function onEvent(debuggeeId, message, params) {
-  if (getCurrentTab().id != debuggeeId.tabId)
-    return;
-
-  if (message == "Network.requestWillBeSent") {
-    var requestDiv = requests[params.requestId];
-    if (!requestDiv) {
-      var requestDiv = document.createElement("div");
-      requestDiv.className = "request";
-      requests[params.requestId] = requestDiv;
-      var urlLine = document.createElement("div");
-      urlLine.textContent = params.request.url;
-      requestDiv.appendChild(urlLine);
-    }
-
-    if (params.redirectResponse)
-      appendResponse(params.requestId, params.redirectResponse);
-
-    var requestLine = document.createElement("div");
-    requestLine.textContent = "\n" + params.request.method + " " +
-        parseURL(params.request.url).path + " HTTP/1.1";
-    requestDiv.appendChild(requestLine);
-    document.getElementById("container").appendChild(requestDiv);
-  } else if (message == "Network.responseReceived") {
-    appendResponse(params.requestId, params.response);
-  }
+// The body of this function will be executed as a content script inside the
+// current page
+function setPageBackgroundColor() {
+  chrome.storage.sync.get("color", ({ color }) => {
+    document.body.style.backgroundColor = color;
+  });
+  console.log('Document background color set to %c%s', `color: ${color}`, color);
 }
